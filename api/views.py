018393a534afc161a229cc162a1a7853bc3cee4a
Basheer Loay <basheer.loay@harri.com>
2025-09-02 19:14:53 +0300

    Make changes

--- api/views.py
+++ api/views.py
@@ -11,7 +11,7 @@ def endpoint_a(request):
     """
     Endpoint A - Returns a simple message.
     """
-    return Response("Really mad ):( from endpoint a!", status=status.HTTP_200_OK)
+    return Response("--> Really mad ):( from endpoint a!", status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
